1d136b93ae8df659d9b6506a22268c23814096ad
indra120 <indrapranata120@gmail.com>
2024-02-05 15:50:21 +0800

    List information

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { shoppingList } from "./store"
   import Navbar from "@/lib/Navbar.svelte"
+  import Info from "@/lib/Info.svelte"
   import ShoppingLists from "@/lib/ShoppingLists.svelte"
 </script>
 
@@ -9,6 +10,8 @@
 <section
   class="flex flex-col h-[540px] max-w-[480px] p-4 my-6 mx-auto bg-gray-100 rounded-xl shadow-md"
 >
+  <Info />
+  
   {#if $shoppingList.length > 0}
     <ShoppingLists />
   {:else}
---
+++ src/lib/Info.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { shoppingList } from "@/store"
+
+  $: totalCounts = $shoppingList.reduce((total, item) => total + item.count, 0)
+</script>
+
+<div class="flex justify-between items-center w-full py-4 px-0">
+  <div class="flex justify-center items-center text-gray-700 text-xs">
+    <p class="font-bold">Total List: {$shoppingList.length}</p>
+  </div>
+
+  <div class="flex justify-center items-center text-gray-700 text-xs">
+    <p class="font-bold">Total Counts: {totalCounts}</p>
+  </div>
+
+  <button
+    on:click={() => shoppingList.set([])}
+    class="bg-[none] border-0 p-0 text-red-500 text-xs font-bold cursor-pointer"
+  >
+    Delete All Items
+  </button>
+</div>

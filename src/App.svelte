bf0fff4445ebc7099e1eb444bfe4c471591765cc
indra120 <indrapranata120@gmail.com>
2024-02-05 16:03:12 +0800

    Create a method to add new item

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { shoppingList } from "./store"
   import Navbar from "@/lib/Navbar.svelte"
+  import AddShoppingItem from "@/lib/AddShoppingItem.svelte"
   import Info from "@/lib/Info.svelte"
   import ShoppingLists from "@/lib/ShoppingLists.svelte"
 </script>
@@ -10,8 +11,9 @@
 <section
   class="flex flex-col h-[540px] max-w-[480px] p-4 my-6 mx-auto bg-gray-100 rounded-xl shadow-md"
 >
+  <AddShoppingItem />
   <Info />
-  
+
   {#if $shoppingList.length > 0}
     <ShoppingLists />
   {:else}
---
+++ src/lib/AddShoppingItem.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { shoppingList } from "@/store"
+
+  let title: string
+
+  const handleSubmit = () => {
+    if (!title) return alert("No title provided!")
+    shoppingList.update((lists) => {
+      lists.unshift({ title, count: 0 })
+      return lists
+    })
+    title = ""
+  }
+</script>
+
+<form class="flex" on:submit|preventDefault={handleSubmit}>
+  <input
+    class="flex-1 mr-2 shadow border-0 rounded-full p-3 text-gray-700 text-base font-semibold outline-0"
+    bind:value={title}
+    type="text"
+    placeholder="List"
+  />
+  <button
+    class="border-0 rounded-full text-slate-50 text-base font-bold uppercase w-20 bg-indigo-400 shadow cursor-pointer"
+    type="submit"
+  >
+    add
+  </button>
+</form>

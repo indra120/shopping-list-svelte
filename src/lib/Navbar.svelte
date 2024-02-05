b9798288938042b2e06a428489d3940885819dfc
indra120 <indrapranata120@gmail.com>
2024-02-05 12:52:57 +0800

    Create Navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,10 @@
+<script>
+  import shoppingIcon from "@/assets/shopping-icon.svg"
+</script>
+
+<nav
+  class="flex justify-center items-center text-center bg-indigo-400 py-0 px-4 h-14"
+>
+  <img src={shoppingIcon} alt="shopping icon" class="size-6" />
+  <h1 class="text-white text-2xl font-bold">Shopping List</h1>
+</nav>
